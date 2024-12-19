c3c6574a4531a678b6c0faa80756498bc19e2bab
zxwin0125 <zxwin_0125@163.com>
2024-12-19 11:11:35 +0800

    feat: 节点不一致

--- src/TinyReact/diff.js
+++ src/TinyReact/diff.js
@@ -1,3 +1,4 @@
+import createDOMElement from './createDOMElement';
 import mountElement from './mountElement';
 import updateNodeElement from './updateNodeElement';
 import updateTextNode from './updateTextNode';
@@ -8,13 +9,19 @@ export default function diff(virtualDOM, container, oldDOM) {
 	if (!oldDOM) {
 		// 如果不存在 不需要对比 直接将 Virtual DOM 转换为真实 DOM
 		mountElement(virtualDOM, container);
+	} else if (
+		virtualDOM.type !== oldVirtualDOM.type &&
+		virtualDOM.type !== 'function'
+	) {
+		const newElement = createDOMElement(virtualDOM);
+		oldDOM.parentNode.replaceChild(newElement, oldDOM);
 	} else if (oldVirtualDOM && virtualDOM.type === oldVirtualDOM.type) {
 		if (virtualDOM.type === 'text') {
 			// 更新文本
 			updateTextNode(virtualDOM, oldVirtualDOM, oldDOM);
 		} else {
 			// 更新元素
-			updateNodeElement(oldDOM, virtualDOM, oldVirtualDOM)
+			updateNodeElement(oldDOM, virtualDOM, oldVirtualDOM);
 		}
 
 		// 对比子节点
